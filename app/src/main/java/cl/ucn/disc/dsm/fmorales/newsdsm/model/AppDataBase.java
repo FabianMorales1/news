bab502c148cae8dda55d5e183e6619e284c60a2e
Fabian Morales <fabian.morales@alumnos.ucn.cl>
2021-01-07 08:19:31 -0300

    feat(Database): created the database class used for the local storage of news

---
+++ app/src/main/java/cl/ucn/disc/dsm/fmorales/newsdsm/model/AppDataBase.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2020 Fabian Morales Araya, fabian.morales@alumnos.ucn.cl
+ * Copyright (c) 2020 Felipe Herrera Encina, felipe.herrera01@alumnos.ucn.cl
+ * Copyright (c) 2020 Diego Duarte Diaz, diego.duarte@alumnos.ucn.cl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.fmorales.newsdsm.model;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+
+import org.threeten.bp.ZonedDateTime;
+
+@Database(entities = {News.class}, version = 1)
+@TypeConverters(Converters.class)
+public abstract class AppDataBase extends RoomDatabase {
+    public abstract NewsDao newsDao();
+}
+
+class Converters {
+
+    @TypeConverter
+    public static ZonedDateTime toDate(String dateString) {
+        if (dateString == null) {
+            return null;
+        } else {
+            return ZonedDateTime.parse(dateString);
+        }
+    }
+
+    @TypeConverter
+    public static String toDateString(ZonedDateTime date) {
+        if (date == null) {
+            return null;
+        } else {
+            return date.toString();
+        }
+    }
+}
